05f12cc77fb93aa75d35ca1e1fca7a419f58b738
Chirag Bitoni <chirag.bitoni@grapecity.com>
2025-11-06 12:22:27 +0530

    Added form on install page

--- src/app/Install/page.jsx
+++ src/app/Install/page.jsx
@@ -1,6 +1,7 @@
 "use client";
 import Hero from "../../app/components/Install/Sections/Hero/Hero"
 import Fifth from "../components/Install/Sections/Fifth/Fifth";
+import InstallForm from "../components/Install/Sections/InstallForm/InstallForm";
 import Fourth from "../components/Install/Sections/Fourth/Fourth";
 import Second from "../components/Install/Sections/Second/Second";
 import Third from "../components/Install/Sections/Third/Third";
@@ -9,6 +10,7 @@ export default function Install() {
     <div >
       <Hero />
       <Second />
+      <InstallForm />
       <Third />
       <Fourth />
       <Fifth />
