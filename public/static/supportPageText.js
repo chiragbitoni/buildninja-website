1376a330cf4c5c4b1d443ff1fbd20bd99b6e6fd1
Chirag Bitoni <chirag.bitoni@grapecity.com>
2025-11-10 17:11:29 +0530

    changed text

--- public/static/supportPageText.js
+++ public/static/supportPageText.js
@@ -1,53 +1,9 @@
 import { paths } from "./paths";
 
-export const heroSectionText = {
-  title: "We’re Here to Help You Build Without Roadblocks",
-  subtitle:
-    "Whether you have a question, need technical help, or want to learn more our team is just a message away.",
-  column1: {
-    title: "Get Help",
-    subTitle: "We're here to help you suceed with BuildNinja",
-    list: [
-      {
-        icon: paths.icons.emailBlue,
-        title: "Email Support",
-        description: "hello@grapehub.io",
-      },
-      {
-        icon: paths.icons.documentpurple,
-        title: "Documentation",
-        description: "Browse guides and tutorials",
-
-      },
-    ],
-  },
-  form: {
-    title: "Contact Us",
-    name: {
-      label: "Your Name",
-      placeholder: "Enter your name",
-    },
-    email: {
-      label: "Email Address",
-      placeholder: "Enter your email address",
-    },
-    sbject: {
-      label: "Subject",
-      placeholder: "How can we help?",
-    },
-    message: {
-      label: "Message",
-      placeholder: "Tell us more about your question or issue...",
-    },
-    chekbox: "I'm not a robot",
-    button: "Send",
-  },
-};
-
 export const supportHeroText = {
-  heading: "Get in Touch with BuildNinja Support",
+  heading: "We’re Here to Help You Build Without Roadblocks",
   subheading:
-    "Whether you have a question, need technical help, or want to learn more — our team is just a message away.",
+    "Whether you have a question, need technical help, or want to learn more our team is just a message away.",
   getHelp: "Get Help",
   helpText: "We’re here to help you succeed with BuildNinja",
   options: [
