07741c9826f8f6cc466acffc81bc6d4cdc1df978
Chirag Bitoni <chirag.bitoni@grapecity.com>
2025-11-10 11:19:00 +0530

    fixed responsiveness

--- src/app/components/Home/Sections/Fifth/Fifth.jsx
+++ src/app/components/Home/Sections/Fifth/Fifth.jsx
@@ -1,29 +1,50 @@
 import "./Fifth.css";
 import { fifthSectionText } from "../../../../../../public/static/homePageText";
 import Image from "next/image";
+
 export default function Fifth() {
     return (
         <section className="fifthHomeSection">
             <div className="fifthHomeContent">
+                {/* FIRST TITLE */}
                 <h2 className="fifthHomeTitle">{fifthSectionText.title}</h2>
+
+                {/* SUBTITLE */}
                 <div className="fifthHomeSubTitle">
-                    <p >{fifthSectionText.subTitle}<br />
-                    {fifthSectionText.subTitle2}</p>
+                    <p>
+                        {fifthSectionText.subTitle}
+                        <br />
+                        {fifthSectionText.subTitle2}
+                    </p>
                 </div>
+
+                {/* CARDS */}
                 <div className="fifthHomeCardsContainer">
                     {fifthSectionText.cards.map((data, index) => (
                         <div className="fifthHomeCard" key={index}>
-                            <Image width={100} height={100} alt={data.text} src={data.icon} className={data.icon.includes("ninty") || data.icon.includes("500") ? "fifthHomeCardIcon1" : "fifthHomeCardIcon2"} />
+                            <Image
+                                width={100}
+                                height={100}
+                                alt={data.text}
+                                src={data.icon}
+                                className={
+                                    data.icon.includes("ninty") || data.icon.includes("500")
+                                        ? "fifthHomeCardIcon1"
+                                        : "fifthHomeCardIcon2"
+                                }
+                            />
                             <div className="fifthHomeCardText">
-                                <p className="text1">{data.boldText ? <strong>{data.boldText}</strong> : null}</p>
-                                <p className="text1">{data.text}</p>
+                                {data.boldText && <strong>{data.boldText}</strong>}
+                                <p>{data.text}</p>
                             </div>
                         </div>
                     ))}
                 </div>
-                <h2 className="fifthHomeTitle">{fifthSectionText.secondTitle}</h2>
+
+                {/* SECOND SECTION TITLES */}
+                <h2 className="fifthHomeSecondTitle">{fifthSectionText.secondTitle}</h2>
                 <h2 className="fifthHomeThirdTitle">{fifthSectionText.thirdTitle}</h2>
             </div>
         </section>
-    )
+    );
 }
