7cd6c8563871bee8ae73084fa8f3a570a53b9695
Chirag Bitoni <chirag.bitoni@grapecity.com>
2025-11-13 20:01:03 +0530

    case sensitivity

---
+++ src/app/features/page.jsx
@@ -0,0 +1,13 @@
+"use client";
+import Hero from "../../app/components/Features/Sections/Hero/Hero"
+import Second from "../components/Features/Sections/Second/Second";
+import Third from "../components/Features/Sections/Third/Third";
+export default function Features() {
+  return (
+    <div >
+      <Hero />
+      <Second />
+      <Third />
+    </div>
+  );
+}
---
+++ src/app/support/page.jsx
@@ -0,0 +1,11 @@
+"use client";
+
+import Hero from "../components/Supports/Sections/Hero/Hero";
+
+export default function Support() {
+  return (
+    <div >
+        <Hero />
+    </div>
+  );
+}
