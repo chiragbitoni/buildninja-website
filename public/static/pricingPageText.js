7019edb32e9e8d7b1263dd347f77dc2779c9eaa8
Chirag Bitoni <chirag.bitoni@grapecity.com>
2025-12-05 16:10:03 +0530

    updated grace period

--- public/static/pricingPageText.js
+++ public/static/pricingPageText.js
@@ -44,7 +44,7 @@ export const secondSectionTextIndia = {
         "Up to 100 configurations",
         "Standard Email support",
         "1 SSO provider integration (choose: Microsoft, GitHub, GitLab, Bitbucket, or Google)",
-        "30-day grace period to test everything",
+        "7-day grace period to test everything",
       ],
       buttonText: "Get Your Free Key Now",
       buttonText2: "Start with Trial License",
