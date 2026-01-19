ebe4bfc099630cca74269c0014cbcb373fc00169
Chirag Bitoni <chirag.bitoni@grapecity.com>
2026-01-19 15:34:53 +0530

    added schema and llms text for SEO

--- src/app/layout.js
+++ src/app/layout.js
@@ -9,7 +9,7 @@ import GoogleAnalytics from "./components/Analytics/GA";
 import YouTubePopup from "./components/YouTubePopup/YouTubePopup";
 import PHProviderWrapper from "./components/Analytics/Providers";
 import PosthogWrapper from "./components/Analytics/PostHogWrapper";
-
+import Script from "next/script";
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -19,7 +19,32 @@ const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
 });
-
+const schema = {
+  "@context": "https://schema.org",
+  "@graph": [
+    {
+      "@type": "Organization",
+      "@id": "https://buildninja.grapehub.io/#organization",
+      name: "BuildNinja",
+      url: "https://buildninja.grapehub.io",
+      logo: "https://buildninja.grapehub.io/resources/BuildNinja.png",
+      sameAs: [
+        "https://www.linkedin.com/company/grapecityindiapvtltd/",
+        "https://www.instagram.com/grapecityindia/",
+        "https://www.facebook.com/GrapeCityIndiaPvtLtd",
+      ],
+    },
+    {
+      "@type": "WebSite",
+      "@id": "https://buildninja.grapehub.io/#website",
+      url: "https://buildninja.grapehub.io",
+      name: "BuildNinja",
+      publisher: {
+        "@id": "https://buildninja.grapehub.io/#organization",
+      },
+    },
+  ],
+};
 export const metadata = {
   title: "BuildNinja – Self hosted CI/CD platform",
   description:
@@ -42,7 +67,7 @@ export const metadata = {
     type: "website",
     images: [
       {
-        url: "https://buildninja.grapehub.io/resources/GrapecityWhite.png",
+        url: "https://buildninja.grapehub.io/resources/BuildNinja.png",
         width: 1200,
         height: 630,
         alt: "BuildNinja CI/CD platform",
@@ -55,7 +80,7 @@ export const metadata = {
     title: "BuildNinja – Self hosted CI/CD platform",
     description:
       "BuildNinja helps developers manage software development life cycle efficiently with advanced CI/CD technology, real-time collaboration, and zero downtime updates.",
-    images: ["https://buildninja.grapehub.io/resources/GrapecityWhite.png"],
+    images: ["https://buildninja.grapehub.io/resources/BuildNinja.png"],
   },
 };
 
@@ -63,6 +88,11 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
+        <Script
+          id="schema-org"
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{ __html: JSON.stringify(schema) }}
+        />
         <GoogleAnalytics GA_ID={process.env.NEXT_PUBLIC_GA_ID} />
       </head>
       <body
