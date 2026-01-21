b71be4b7f6b2f85a3149ccd463f8392f8adb9c71
Chirag Bitoni <chirag.bitoni@grapecity.com>
2026-01-21 16:57:15 +0530

    Addded chat bot

--- src/app/layout.js
+++ src/app/layout.js
@@ -111,6 +111,23 @@ export default function RootLayout({ children }) {
             </ClientAuthProvider>
           </ReduxProvider>
         </PHProviderWrapper>
+        <Script
+          id="tawk-to"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+                var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+                (function(){
+                var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+                s1.async=true;
+                s1.src='https://embed.tawk.to/6970af33f657ac197b7847f7/1jfg2op82';
+                s1.charset='UTF-8';
+                s1.setAttribute('crossorigin','*');
+                s0.parentNode.insertBefore(s1,s0);
+              })();
+            `,
+          }}
+        />
       </body>
     </html>
   );
