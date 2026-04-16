8e99f19ca9940d2f9fe30c980197b58d19dd0068
Chirag Bitoni <chirag.bitoni@grapecity.com>
2026-04-16 10:16:57 +0530

    rm talk to sales

--- src/components/Features/Sections/FeatureCta/FeatureCta.jsx
+++ src/components/Features/Sections/FeatureCta/FeatureCta.jsx
@@ -46,7 +46,7 @@ export default function FeatureCta() {
               <svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2.2" strokeLinecap="round" strokeLinejoin="round">
                 <path d="M21 15a2 2 0 0 1-2 2H7l-4 4V5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2z"/>
               </svg>
-              Talk to Sales
+              Contact Us
             </a>
           </div>
 
